6e7056a886b90592255264186b8b91475667a4f5
Nate Weaver <wevah@derailer.org>
2019-10-29 16:39:23 -0500

    Highlight matched substrings when filtering
    - Also set the global flag on the RegExp.

--- js/filter.js
+++ js/filter.js
@@ -3,9 +3,10 @@
 function filterGenerators(str) {
 	let generators = document.getElementsByClassName('generator-switcher');
 	let toKeep;
+	let regex;
 	
 	try {
-		let regex = new RegExp(str, 'i');
+		regex = new RegExp(str, 'ig');
 		toKeep = Array.from(generators).filter(x => regex.test(x.innerText));
 	} catch (error) {
 		toKeep = generators;
@@ -13,11 +14,24 @@ function filterGenerators(str) {
 	
 	for (let x of generators) {
 		x.style.display = 'none';
+		resetMatchHighlight(x)
 	}
 	
 	for (let x of toKeep) {
 		x.style.display = 'inline-block';
-	}	
+		
+		if (regex)
+			highlightMatchInElement(regex, x);
+	}
+}
+
+function resetMatchHighlight(element) {
+	element.innerHTML = element.innerText;
+}
+
+function highlightMatchInElement(regex, element) {
+	let text = element.innerText;
+	element.innerHTML = element.innerText.replace(regex, '<span class="filter-highlight">$&</span>');
 }
 
 document.addEventListener('DOMContentLoaded', e => {
